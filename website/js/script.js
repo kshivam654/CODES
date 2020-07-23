65b51bb905a8a157f6f957d6c84972f81e7fa35f
Shivam Sharma <kshivam654@gmail.com>
2020-07-23 09:36:56 +0530

    fontawesome4 added along with google fonts

--- website/js/script.js
+++ website/js/script.js
@@ -27,19 +27,23 @@ this is an example to multi-line comment.
 // $('h1').hide(500).show(500)
 
 
+// $(function(){
+// // this will execute when the webpage is ready to execute
+//     $('h1').slideUp(1000).slideDown(1000)
+
+//     $('h1').click(function(){
+//         $('.content').css({
+//             color: 'blue',
+//             fontSize: 50
+//         })
+//     })
+// })
+
+// $(window).on('load', function(){
+//     //when the window is fully loaded means the content the image the all files of the window is totally ready including graphics while the document().ready() is invloked a bit earlier.
+//     // alert('window is loaded')
+// })
+
 $(function(){
-// this will execute when the webpage is ready to execute
-    $('h1').slideUp(1000).slideDown(1000)
-
-    $('h1').click(function(){
-        $('.content').css({
-            color: 'blue',
-            fontSize: 50
-        })
-    })
-})
-
-$(window).on('load', function(){
-    //when the window is fully loaded means the content the image the all files of the window is totally ready including graphics while the document().ready() is invloked a bit earlier.
-    // alert('window is loaded')
+    
 })
