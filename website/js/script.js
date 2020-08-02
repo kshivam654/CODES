ab1a783b8459fc0bf2bddbcc584eb685d672bf47
Shivam Sharma <kshivam654@gmail.com>
2020-08-03 00:54:28 +0530

    owl crousel added

--- website/js/script.js
+++ website/js/script.js
@@ -5,3 +5,19 @@ $(window).on('load', function(){
     $('#status').fadeOut()
     $('#preloader').delay(350).fadeOut('slow')
 })
+
+/*============================================
+                    Owl-Carousel --> Team
+=============================================*/
+$(function(){
+    $('#team-members').owlCarousel({
+        items: 2,
+        autoplay: true,
+        smartSpeed: 700,
+        loop: true,
+        autoplayHoverPause: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>']
+    })
+})
